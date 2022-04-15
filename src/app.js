4c33f41b6c7b7e94ee85e428e6b01d3b6b4dd9db
Lupey1103 <luperon.christian95@gmail.com>
2022-04-15 00:05:07 +0000

    Felix the Excuse Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Doom Guy", "Master Chief", "Kratos", "Captain Price"];
+  let action = ["dismembered", "blew up", "killed", "360 no scoped"];
+  let what = ["a demon", "the Gods", "the Covenant"];
+  let when = [
+    "before the class",
+    "20 minutes ago",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function excusesForWork() {
+    document.getElementById("excuse").innerHTML =
+      who[Math.floor(Math.random() * who.length)] +
+      " " +
+      action[Math.floor(Math.random() * action.length)] +
+      " " +
+      what[Math.floor(Math.random() * what.length)] +
+      " " +
+      when[Math.floor(Math.random() * when.length)];
+  }
+
+  document.getElementById("myBtn").addEventListener("click", excusesForWork);
+
+  console.log("They Called Him the Excuse Slayer!");
 };
